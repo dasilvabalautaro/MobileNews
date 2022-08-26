58da824289880278b141568f94d0a5f2a6cf7a82
Arturo Silva <david.arturo.silva@gmail.com>
2022-08-26 02:03:07 -0400

    Initial commit, configuration of libraries and network service

---
+++ app/src/main/java/com/globalhiddenodds/mobilenews/MobileNews.kt
@@ -0,0 +1,7 @@
+package com.globalhiddenodds.mobilenews
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MobileNews: Application()
---
+++ app/src/main/java/com/globalhiddenodds/mobilenews/datasource/network/CloudService.kt
@@ -0,0 +1,15 @@
+package com.globalhiddenodds.mobilenews.datasource.network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+private const val baseUrl = "https://hn.algolia.com/api/v1/search_by_date"
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(baseUrl)
+    .build()
