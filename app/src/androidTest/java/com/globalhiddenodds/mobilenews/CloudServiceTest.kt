e6eb1712d6177e0bcc053b598d53b59ad03b5179
Arturo Silva <david.arturo.silva@gmail.com>
2022-08-26 08:01:12 -0400

    Add libraries of test and CloudService tested

---
+++ app/src/androidTest/java/com/globalhiddenodds/mobilenews/CloudServiceTest.kt
@@ -0,0 +1,62 @@
+package com.globalhiddenodds.mobilenews
+
+import com.globalhiddenodds.mobilenews.FileReader.readStringFromFile
+import com.globalhiddenodds.mobilenews.datasource.network.INewsCloudService
+import com.globalhiddenodds.mobilenews.workers.WorkerUtils
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.runBlocking
+import mockwebserver3.MockResponse
+import mockwebserver3.MockWebServer
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.net.HttpURLConnection
+
+class CloudServiceTest {
+    @get:Rule
+    var rule = OkHttpIdlingResourceRule()
+    private val mockWebServer = MockWebServer()
+    private lateinit var cloudService: INewsCloudService
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+    @Before
+    fun setup() {
+        mockWebServer.start()
+        cloudService = Retrofit.Builder()
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .baseUrl(mockWebServer.url("/"))
+            .build()
+            .create(INewsCloudService::class.java)
+    }
+    @After
+    fun teardown() {
+        mockWebServer.shutdown()
+    }
+    @Test
+    fun shouldShowResponseCorrectly() = runBlocking {
+        mockWebServer.enqueue(
+            MockResponse()
+                .setResponseCode(HttpURLConnection.HTTP_OK)
+                .setBody(readStringFromFile("success_response.json"))
+        )
+        val response = cloudService.getNews()
+        @Suppress("UNCHECKED_CAST")
+        val jsonMap: MutableMap<String?, Any> = response as MutableMap<String?, Any>
+        val hits = WorkerUtils.getHits(jsonMap)
+        val listHitCloud = WorkerUtils.setHits(hits)
+        assert(listHitCloud.isNotEmpty())
+    }
+
+    @Test
+    fun shouldShowError() {
+        val response = MockResponse()
+            .setResponseCode(HttpURLConnection.HTTP_INTERNAL_ERROR)
+        mockWebServer.enqueue(response)
+        assert(!response.status.contains("200"))
+    }
+}
--- app/src/main/java/com/globalhiddenodds/mobilenews/datasource/network/CloudService.kt
+++ app/src/main/java/com/globalhiddenodds/mobilenews/datasource/network/CloudService.kt
@@ -4,8 +4,10 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
 
-private const val baseUrl = "https://hn.algolia.com/api/v1/search_by_date"
+private const val baseUrl = "https://hn.algolia.com/api/v1/"
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
     .build()
@@ -13,3 +15,13 @@ private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(baseUrl)
     .build()
+interface INewsCloudService {
+    @GET("search_by_date?query=android")
+    suspend fun getNews(): Any
+}
+//Pattern Singleton
+object NewsCloud {
+    val retrofitService: INewsCloudService by lazy {
+        retrofit.create(INewsCloudService::class.java)
+    }
+}
